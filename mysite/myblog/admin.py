635b76f3307afaf656e756c10342c7e2b851481b
Stephen <ssoukasene@gmail.com>
2018-08-06 00:26:45 -0700

    Added Category editor to Post view and removed Post selector from Category view

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
+# Create inline editing template for Category in Post Admin
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+
+# Exclude posts from category admin window
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
